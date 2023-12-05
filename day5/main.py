2c2d5b7c4c89d6c8c744a42ab33ec389458b891b
Pochingto <rcwk3756@gmail.com>
2023-12-05 16:17:39 -0500

    just lint formatting

--- day5/main.py
+++ day5/main.py
@@ -2,19 +2,22 @@ def map_source_to_dest(src: int, src_dest_map: list[tuple[int, int, int]]) -> in
     for dest_start, src_start, length in src_dest_map:
         if src in range(src_start, src_start + length):
             return dest_start + (src - src_start)
-        
+
     return src
+
 
 def parse_map(line: list[str]) -> tuple[int, int, int]:
     # print("called parse map")
     return tuple(int(num.strip()) for num in line.strip().split(" ") if len(num) > 0)
 
+
 def parse_seed(line: str) -> list[int]:
-    seeds = line[line.find(":") + 1:]
+    seeds = line[line.find(":") + 1 :]
     seeds = [int(seed.strip()) for seed in seeds.strip().split(" ") if len(seed) > 0]
     return seeds
 
-def calculate_lowest_location(lines: list[str]) -> int: 
+
+def calculate_lowest_location(lines: list[str]) -> int:
     seeds = parse_seed(lines[0])
     MAPS = [
         "seed-to-soil",
@@ -24,12 +27,12 @@ def calculate_lowest_location(lines: list[str]) -> int:
         "light-to-temperature",
         "temperature-to-humidity",
         "humidity-to-location",
-        "dummy"
+        "dummy",
     ]
 
     lowest_location = float("inf")
     lines = lines[2:]
-    for seed in seeds: 
+    for seed in seeds:
         # print(f"================== seed: {seed}==================")
         current = 0
         maps = []
@@ -39,7 +42,7 @@ def calculate_lowest_location(lines: list[str]) -> int:
         for line in lines:
             # print(f"line: {line}, len: {len(line)}")
             # print(f"{line} . startswith {MAPS[current]} : {line.startswith(MAPS[current])}")
-            if len(line) <= 1: 
+            if len(line) <= 1:
                 src = map_source_to_dest(src, maps)
                 # print(f"maps: {maps}")
                 # print(f"src changed to {src}")
@@ -48,24 +51,21 @@ def calculate_lowest_location(lines: list[str]) -> int:
                 current += 1
             elif starting_map:
                 maps.append(parse_map(line))
-            elif line.startswith(MAPS[current]): 
+            elif line.startswith(MAPS[current]):
                 # print(f"Current: {MAPS[current]}")
                 starting_map = True
 
         # print(f"location: {src}")
-        if src < lowest_location: 
+        if src < lowest_location:
             lowest_location = src
 
     return lowest_location
 
+
 if __name__ == "__main__":
-    with open("input.txt", "r", encoding="utf-8") as f: 
+    with open("input.txt", "r", encoding="utf-8") as f:
         lines = f.readlines()
         lowest_location = calculate_lowest_location(lines)
         print(f"lowest: {lowest_location}")
 
         # 640121624 too high
-
-        
-
-        
