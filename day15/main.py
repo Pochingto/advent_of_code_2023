3d8b50190b75de3c4aa1617450f62bafcdb564d0
Pochingto <rcwk3756@gmail.com>
2023-12-15 18:52:07 -0500

    day 15 part 2

--- day15/main.py
+++ day15/main.py
@@ -8,6 +8,43 @@ def hash(string: str) -> int:
     return current
 
 
+def part2_hash_map(strings: str) -> list[list[tuple[str, int]]]:
+    boxes = [[] for _ in range(256)]
+
+    for string in strings:
+        op_idx = string.find("=")
+        if op_idx == -1:
+            op_idx = string.find("-")
+
+        label, op = string[:op_idx], string[op_idx]
+        box_idx = hash(label)
+        if op == "-":
+            for l, f in boxes[box_idx]:
+                if l == label:
+                    boxes[box_idx].remove((l, f))
+        elif op == "=":
+            focal = int(string[-1])
+            exists = False
+            for i, (l, f) in enumerate(boxes[box_idx]):
+                if l == label:
+                    boxes[box_idx][i] = (label, focal)
+                    exists = True
+                    break
+            if not exists:
+                boxes[box_idx].append((label, focal))
+
+    return boxes
+
+
+def calculate_focal_power(boxes: list[list[tuple[str, int]]]) -> int:
+    focal_power = 0
+    for i, box in enumerate(boxes):
+        for j, (_, focal) in enumerate(box):
+            focal_power += (i + 1) * (j + 1) * focal
+
+    return focal_power
+
+
 def parse_line(line: str) -> list[str]:
     return [string.strip() for string in line.strip().split(",")]
 
@@ -23,3 +60,8 @@ if __name__ == "__main__":
         # part 1
         sum_hash = calculate_sum_of_hash(strings)
         print(f"Sum of hash: {sum_hash}")
+
+        # part2
+        boxes = part2_hash_map(strings)
+        focal_power = calculate_focal_power(boxes)
+        print(f"focal power: {focal_power}")
--- day15/test_main.py
+++ day15/test_main.py
@@ -1,7 +1,6 @@
 from main import *
 
-def test_hash():
-    assert hash("HASH") == 52
+def test_part1_hash():
     assert hash("rn=1") == 30
     assert hash("cm-") == 253
     assert hash("qp=3") == 97
@@ -14,3 +13,31 @@ def test_hash():
     assert hash("pc=6") == 214
     assert hash("ot=7") == 231
     
+def test_part2_hash_map(): 
+    strings = [
+        "rn=1", 
+        "cm-", 
+        "qp=3", 
+        "cm=2", 
+        "qp-", 
+        "pc=4", 
+        "ot=9",
+        "ab=5", 
+        "pc-", 
+        "pc=6", 
+        "ot=7"
+    ]
+
+    boxes = part2_hash_map(strings)
+    assert boxes[0] == [("rn", 1), ("cm", 2)]
+    assert boxes[3] == [("ot", 7), ("ab", 5), ("pc", 6)]
+    for i, box in enumerate(boxes): 
+        if i != 0 and i != 3: 
+            assert len(box) == 0
+
+def test_calculate_focal_power(): 
+    boxes = [[] for _ in range(256)]
+    boxes[0] = [("rn", 1), ("cm", 2)]
+    boxes[3] = [("ot", 7), ("ab", 5), ("pc", 6)]
+
+    assert calculate_focal_power(boxes) == 145
